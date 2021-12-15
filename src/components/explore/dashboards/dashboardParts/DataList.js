491d48222625cb9cc36d66bbe96ce7858de25996
Peter Kok <kretep@users.noreply.github.com>
2021-12-15 13:59:29 +0100

    Make deletion modal a separate component

--- src/components/explore/dashboards/dashboardParts/DataList.js
+++ src/components/explore/dashboards/dashboardParts/DataList.js
@@ -2,8 +2,9 @@ import db from "apis/dexie";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 import {
-  Button, ButtonGroup, Checkbox, Container, Dimmer, Header, Loader, Modal, Segment, Table, Visibility
+  Button, Container, Dimmer, Header, Loader, Segment, Table, Visibility
 } from "semantic-ui-react";
+import ConfirmModal from "./ConfirmModal";
 
 const PAGESIZE = 25;
 
@@ -127,58 +128,6 @@ const DataList = ({ table, layout, selection, loading }) => {
   );
 };
 
-const ConfirmModal = ({ table, confirm, setConfirm }) => {
-  const [ask, setAsk] = useState(!confirm.ask);
-  const n = confirm.itemIds.length;
-
-  const handleDelete = async () => {
-    await db.deleteTableIds(table, confirm.itemIds);
-    setConfirm({ open: false, ask: !ask, itemIds: [] });
-  };
-
-  if (!confirm.open) return null;
-
-  if (!confirm.ask && n === 1) {
-    handleDelete();
-    return null;
-  }
-
-  return (
-    <Modal
-      style={{ backgroundColor: "#00000054" }}
-      open={confirm.open}
-      onClose={() => setConfirm({ ...confirm, open: false })}
-    >
-      <Modal.Header>Delete item{n === 1 ? "" : "s"}</Modal.Header>
-      <Modal.Content>
-        <p>Are you sure you want to delete {n === 1 ? "this item" : `${n} items`}?</p>
-
-        <br />
-        <Modal.Actions>
-          <ButtonGroup centered>
-            <Button
-              fluid
-              primary
-              onClick={() => setConfirm({ open: false, ask: !ask, itemIds: [] })}
-            >
-              Cancel
-            </Button>
-            <Button fluid color="red" onClick={(e, d) => handleDelete()}>
-              Yes
-            </Button>
-          </ButtonGroup>
-          {n > 1 ? null : (
-            <Checkbox
-              style={{ float: "right" }}
-              onChange={(e, d) => setAsk(!d.value)}
-              label="Do not ask again. Next time, delete immediately when clicking the trash icon"
-            />
-          )}
-        </Modal.Actions>
-      </Modal.Content>
-    </Modal>
-  );
-};
 
 const fetchFromDb = async (table, pageSize, setN, setSelectionN, setData, selection) => {
   let n = await db.getTableN(table);
